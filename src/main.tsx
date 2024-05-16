9e4dbc48d18e8ef670af1368ed4a93d338db1011
Kirk Hodgman <kjhodgman@gmail.com>
2024-05-16 08:02:53 -0700

    add prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./app.tsx";
-import "./styles/main.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app.tsx';
+import './styles/main.css';
 
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 
-if (!root) throw new Error("No root element found");
+if (!root) {
+  throw new Error('No root element found in `index.html`');
+}
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
